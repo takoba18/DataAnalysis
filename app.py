393af8299c7bf0532df6629f9b9fd6867d4ca83e
takoba18 <takoba18@freeuni.edu.ge>
2022-11-30 01:09:50 +0400

    test

--- app.py
+++ app.py
@@ -14,13 +14,15 @@ st.sidebar.selectbox(
     ('Mobile', 'Web-Based', 'Male', 'Female')
 )
 data=pd.read_csv(r'C:\Users\sote\Downloads\WhatsgoodlyData-10.csv')
-chart_female = data.loc[(data["Segment Type"]=="Gender") & (data["Segment Description"]=="Female respondents")].groupby("Answer")["Count"].sum()
-labels = chart_female.index
-sizes = chart_female.values
-explode = (0, 0.1, 0, 0)
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')
-st.pyplot(fig1)
+
+chart_female = data.loc[(data["Segment Type"]=="Gender") & (data["Segment Description"]=="Female respondents")].groupby("Answer")["Count"].sum()
+#labels = chart_female.index
+#sizes = chart_female.values
+#explode = (0, 0.1, 0, 0)
+
+#fig1, ax1 = plt.subplots()
+#ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+ #       shadow=True, startangle=90)
+#ax1.axis('equal')
+#st.pyplot(fig1)*/
